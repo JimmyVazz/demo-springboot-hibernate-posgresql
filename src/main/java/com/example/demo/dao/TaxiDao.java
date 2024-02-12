9e023db2918118966697d6ccd4fadde0a7764468
Jimmy Vasquez <cjvasquez_98@hotmail.com>
2024-02-12 16:43:21 -0600

    se agrego paginacion

--- src/main/java/com/example/demo/dao/TaxiDao.java
+++ src/main/java/com/example/demo/dao/TaxiDao.java
@@ -3,7 +3,5 @@ package com.example.demo.dao;
 import com.example.demo.models.Taxi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TaxiDao extends JpaRepository<Taxi, Integer> {
 }
--- src/main/java/com/example/demo/services/TaxiService.java
+++ src/main/java/com/example/demo/services/TaxiService.java
@@ -1,11 +1,11 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Taxi;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface TaxiService {
-
-    public List<Taxi> findAll();
-
+    Page<Taxi> findAll(Pageable pageable);
 }
